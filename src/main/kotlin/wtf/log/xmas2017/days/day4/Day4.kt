6060704a898fb238b604563b5dbb1d880e867030
Damian Wieczorek <damian@robinhood.com>
2017-12-04 21:07:19 -0800

    reformat some files according to style
    I have different rules on different computers :\

--- src/main/kotlin/wtf/log/xmas2017/days/day4/Day4.kt
+++ src/main/kotlin/wtf/log/xmas2017/days/day4/Day4.kt
@@ -6,23 +6,23 @@ import wtf.log.xmas2017.openResource
 
 object Day4 : Solver<Int, Int> {
 
-  private fun readInput(): Sequence<List<String>> {
-    return openResource("Day4.txt")
-        .lineSequence()
-        .map { it.split(' ') }
-  }
+    private fun readInput(): Sequence<List<String>> {
+        return openResource("Day4.txt")
+                .lineSequence()
+                .map { it.split(' ') }
+    }
 
-  private fun part1(): Int = readInput().count { it.distinct() == it }
+    private fun part1(): Int = readInput().count { it.distinct() == it }
 
-  private fun part2(): Int = readInput()
-      .map { words ->
-        words.map { it.asSequence().sorted().joinToString("") }
-      }
-      .count { it.distinct() == it }
+    private fun part2(): Int = readInput()
+            .map { words ->
+                words.map { it.asSequence().sorted().joinToString("") }
+            }
+            .count { it.distinct() == it }
 
-  override fun solve() = Solution(
-      part1 = part1(),
-      part2 = part2()
-  )
+    override fun solve() = Solution(
+            part1 = part1(),
+            part2 = part2()
+    )
 
 }
